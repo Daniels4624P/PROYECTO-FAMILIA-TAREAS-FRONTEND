749759e341dbfef7a6220a07ace61dd3384b21ad
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-18 17:17:34 -0500

    Update Incomes.jsx

--- src/pages/Incomes.jsx
+++ src/pages/Incomes.jsx
@@ -48,7 +48,7 @@ const Incomes = () => {
   const onSubmit = async (data) => {
     const formattedData = {
       ...data,
-      valor: unformatNumber(data.valor),
+      valor: Number(unformatNumber(data.valor)),
       cuentaId: Number(data.cuentaId),
     }
 
