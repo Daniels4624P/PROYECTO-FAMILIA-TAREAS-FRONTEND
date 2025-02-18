ae063cbff0af567aee0d9ef6767a3694b29c2712
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-17 19:40:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumenta el límite a 1 MB
+  },
   plugins: [react()],
   resolve: {
     alias: {
