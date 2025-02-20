d37c79f751e6b6a502ce6cc511db9e125c090372
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-19 19:28:30 -0500

    Update numberFormat.js

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -1,23 +1,22 @@
 export const formatNumber = (value) => {
-  if (!value) return ""
-  // Eliminar cualquier carácter que no sea dígito
-  const cleanValue = value.replace(/[^\d]/g, "")
-  // Si no hay dígitos, devolver una cadena vacía
-  if (cleanValue === "") return ""
-  // Convertir a número
-  const number = Number.parseInt(cleanValue, 10)
-  // Formatear con puntos como separadores de miles
-  const formattedNumber = number.toLocaleString("es-CO")
-  // Añadir el símbolo de pesos colombianos al final
-  return `${formattedNumber} COP`
+  if (!value) {
+    return "0.00"
+  }
+
+  const number = parseFloat(value) // Convertir a número para manejar negativos
+  const formattedNumber = number.toLocaleString("es-ES", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+
+  return formattedNumber
 }
 
 export const unformatNumber = (value) => {
-  if (!value) return ""
-  // Eliminar el símbolo de peso, los puntos y cualquier espacio
-  return value
-    .replace(/\s?COP/g, "")
-    .replace(/\./g, "")
-    .trim()
-}
+  if (!value) {
+    return "0"
+  }
 
+  const number = parseFloat(value.replace(/[^0-9,-]/g, '').replace(',', '.'));
+  return number.toString();
+};
